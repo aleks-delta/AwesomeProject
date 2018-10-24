9e7064b2bc4f67f3b06735233326e5945b37b3ee
Aleks D <indrikis@yahoo.com>
2018-10-24 07:24:02 +0200

    connector properties derived from agents instead of scripted

---
+++ Assets/Scripts/Connector.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Connector : MonoBehaviour
+{
+    public Connector(GameObject a1, GameObject a2)
+    {
+        //this.me = m;
+        this.agent1 = a1;
+        this.agent2 = a2;
+        me = CreateConnectorGO(agent1, agent2);
+    }
+
+    public void Update()
+    {
+        LineRenderer lr = me.GetComponent<LineRenderer>();
+        lr.SetPosition(0, agent1.transform.position);
+        lr.SetPosition(1, agent2.transform.position);
+    }
+
+    private GameObject CreateConnectorGO(GameObject agent1, GameObject agent2)
+    {
+        var theConnectorGO = new GameObject("bond " + agent1.name + " + " + agent2.name);
+        theConnectorGO.AddComponent<LineRenderer>();
+
+        theConnectorGO.SetActive(true);
+        var lineRenderer = theConnectorGO.GetComponent<LineRenderer>();
+        lineRenderer.material = Resources.Load("LineMaterial") as Material;
+
+        Color col = new Color(1, 1, 1) {a = 1.0f}; // colors[connections.Count];
+        lineRenderer.startColor = col;
+        lineRenderer.endColor = col;
+        const float MAX_HOTNESS = 10.0f;
+        //thickness proportional to the OTHER person's hotness
+        float thickness1 = agent2.GetComponent<AgentController>().hotness / MAX_HOTNESS; 
+        float thickness2 = agent1.GetComponent<AgentController>().hotness / MAX_HOTNESS;
+        lineRenderer.startWidth = thickness1;
+        lineRenderer.endWidth = thickness2;
+
+        theConnectorGO.transform.position = (agent1.transform.position + agent2.transform.position) / 2.0f;
+        theConnectorGO.transform.localScale = -(agent1.transform.position - agent2.transform.position) / 8.0f;
+        return theConnectorGO;
+    }
+
+    private GameObject me;
+    private GameObject agent1;
+    private GameObject agent2;
+
+    internal void SetActive(bool v)
+    {
+        me.SetActive(v);
+    }
+}
--- Assets/Scripts/PlayingField.cs
+++ Assets/Scripts/PlayingField.cs
@@ -1,30 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-
-public class Connector
-{
-    public Connector(GameObject m, GameObject a1, GameObject a2)
-    {
-        this.me = m;
-        this.agent1 = a1;
-        this.agent2 = a2;
-    }
-    public void UpdatePos()
-    {
-        LineRenderer lr = me.GetComponent<LineRenderer>();
-        lr.SetPosition(0, agent1.transform.position);
-        lr.SetPosition(1, agent2.transform.position);
-    }
-    private GameObject me;
-    private GameObject agent1;
-    private GameObject agent2;
-
-    internal void SetActive(bool v)
-    {
-        me.SetActive(v);
-    }
-}
 
 public class PlayingField : MonoBehaviour {
     public Color[] colors;
@@ -43,50 +18,31 @@ public class PlayingField : MonoBehaviour {
 	void Update () {
 		foreach (var connector in connections)
         {
-            connector.UpdatePos();
+            connector.Update();
         }
 	}
 
     public void AddAgent(GameObject newAgent)
     {
-        Debug.LogFormat("num agents = {0} ", agents.Count);
+        Debug.LogFormat("num agents {0} ", agents.Count);
         agents.Add(newAgent);
         Debug.LogFormat("num agents after = {0} ", agents.Count);
     }
 
-    public Connector AddConnection(GameObject agent1, GameObject agent2, float thickness1 = 1.0f, float thickness2 = 1.0f)
+    public Connector AddConnection(GameObject agent1, GameObject agent2)
     {
-        Debug.Log("Adding connection");
         if (string.Compare(agent1.name, agent2.name) < 0)
         {
             Debug.LogFormat("connection between {0} and {1} skipped", agent1.name, agent2.name);
             return null;
         }
-        Debug.Log("playing sound");
-        audioContainer = this.GetComponent<AudioSource>();
+        Debug.Log("Adding connection");
+        audioContainer = GetComponent<AudioSource>();
         audioContainer.PlayOneShot(joinClip);
 
         Debug.LogFormat("Connection #{0} created", connections.Count);
-        var theConnectorGO = new GameObject("bond " + agent1.name + " + " + agent2.name);
-        theConnectorGO.AddComponent<LineRenderer>();
-        
-        theConnectorGO.SetActive(true);
-        var lineRenderer = theConnectorGO.GetComponent<LineRenderer>();
-        lineRenderer.material = Resources.Load("LineMaterial") as Material;
-
-        //lineRenderer.widthMultiplier = 0.5f;
-
-        Color col = colors[connections.Count];
-        col.a = 1.0f;
-        lineRenderer.startColor = col;
-        lineRenderer.endColor = col;
-        lineRenderer.startWidth = thickness1;
-        lineRenderer.endWidth = thickness2;
-
-        theConnectorGO.transform.position = (agent1.transform.position + agent2.transform.position) / 2.0f;
-        theConnectorGO.transform.localScale = -(agent1.transform.position - agent2.transform.position) / 8.0f;
-
-        var theRealConnection = new Connector(theConnectorGO, agent1, agent2);
+          
+        var theRealConnection = new Connector(agent1, agent2);
         connections.Add(theRealConnection);
         return theRealConnection;
     }
